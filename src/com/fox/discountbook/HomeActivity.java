f339458ff5423fb5bc896dcc092d9ad31e9c3b1d
Shiyi Guo <daisy.shiyiguo@gmail.com>
2014-11-07 18:33:45 -0500

    initial settings for the home page

--- src/com/fox/discountbook/HomeActivity.java
+++ src/com/fox/discountbook/HomeActivity.java
@@ -17,7 +17,7 @@ public class HomeActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.home, menu);
-		return true;
+		return super.onCreateOptionsMenu(menu);
 	}
 
 	@Override
@@ -26,9 +26,9 @@ public class HomeActivity extends ActionBarActivity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
+//		if (id == R.id.action_settings) {
+//			return true;
+//		}
 		return super.onOptionsItemSelected(item);
 	}
 }
